ec92684b1389dbd68eb536a1eed359d25d761c4f
Andres Gomez <andresxv98@gmail.com>
2025-04-03 06:46:14 -0500

    add auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent} from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent, title: 'Login' },
   { path: 'register', component: RegisterComponent, title: 'Register' },
+  //Implementar authGuard en rutas protegidas , canActivate: [authGuard]
 ];
